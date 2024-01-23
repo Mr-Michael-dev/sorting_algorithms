19e6efb30a980a788248ff0e125f058851c2cfa6
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-23 22:04:49 +0100

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integer in ascending order
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Returns: nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, minVal;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minVal = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minVal])
+			{
+				minVal = j;
+			}
+		}
+		if (minVal != i)
+		{
+			temp = array[i];
+			array[i] = array[minVal];
+			array[minVal] = temp;
+		}
+		print_array(array, size);
+	}
+}
