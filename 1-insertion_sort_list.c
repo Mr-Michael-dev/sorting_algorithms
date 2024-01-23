8f3dd15193dd88cf8108cf764f30d991e9bf6645
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-23 13:15:58 +0100

    insertion sort list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,59 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list
+ * insertion_sort_list - sorts a doubly linked list in ascending order
  * @list: pointer to the head of the list
  *
- * Return: nothing
+ * Description: This function sorts a doubly linked list using the
+ * Insertion Sort algorithm. It swaps nodes to rearrange the list in
+ * ascending order and prints the list after each swap.
  */
-
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *sorted;
+	listint_t *unsorted;
 	listint_t *current;
+	listint_t *next;
 
-	if (*list == NULL || (*list)->next == NULL)
-	{
-		return;
-	}
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	while (*list != NULL)
-	{
-		current = *list;
-		*list = (*list)->next;
+    sorted = NULL;  // Sorted part of the list
+    unsorted = *list;
 
-		while (current != NULL && ((*list) == NULL || current->n < (*list)->n))
-		{
-			current->next = (*list);
-			(*list)->prev = current;
-			(*list) = current;
+    while (unsorted != NULL)
+    {
+        next = unsorted->next;
 
-			print_list(*list);
-		}
+        if (sorted == NULL || sorted->n >= unsorted->n)
+        {
+            unsorted->next = sorted;
+            unsorted->prev = NULL;
 
-		list = &((*list)->next);
-	}
+            if (sorted != NULL)
+                sorted->prev = unsorted;
+
+            sorted = unsorted;
+        }
+        else
+        {
+            current = sorted;
+
+            while (current->next != NULL && current->next->n < unsorted->n)
+                current = current->next;
+
+            unsorted->next = current->next;
+            unsorted->prev = current;
+
+            if (current->next != NULL)
+                current->next->prev = unsorted;
+
+            current->next = unsorted;
+        }
+
+        *list = sorted;
+        print_list(*list);
+
+        unsorted = next;
+    }
 }
