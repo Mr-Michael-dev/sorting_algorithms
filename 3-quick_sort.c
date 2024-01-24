ba525765c635afcb851ff75935f342fe28647731
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-24 05:40:04 +0100

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * quick-sort - sorts an array of integers in ascending order
+ * @arrray: array to be sorted
+ * @size: size of the array
+ *
+ * Returns: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	size_t pivot = size - 1;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i; j < size - i - 1; j++)
+		{
+			if (array[pivot] < array[pivot - 1])
+			{
+				temp = array[pivot];
+				array[pivot] = array[pivot - 1];
+				array[pivot - 1] = temp;
+				print_array(array, size);
+			}
+			quick_sort(array, size - 1);
+		}
+	}
+}
